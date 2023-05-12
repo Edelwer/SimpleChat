21cbb22663f7f4e5c8f2ba0dd35c42b1edead9c0
Anton Chernyshkov <edelwer@yandex.ru>
2023-05-12 15:29:28 +0500

    Add setUpNetworking() function.
    It allows connect to server. But only for reading. We can't write messages yet.

--- src/main/java/org/example/SimpleChatClient.java
+++ src/main/java/org/example/SimpleChatClient.java
@@ -2,11 +2,25 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.channels.Channels;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SimpleChatClient {
     private JTextArea outgoing;
     private JTextArea incoming;
     private JButton sendButton;
+    private BufferedReader reader;
+    private InetAddress clientIP;
+
     public void go(){
         JScrollPane scroller = createScrollableTextArea();
 
@@ -40,6 +54,20 @@ public class SimpleChatClient {
         scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
         scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         return scroller;
+    }
+
+    private void setUpNetworking(){
+        try {
+            InetSocketAddress serverAddress = new InetSocketAddress("",5000); // you MUST add necessary IP and port of server
+            SocketChannel socketChannel = SocketChannel.open(serverAddress);
+            clientIP = InetAddress.getLocalHost();
+
+            reader = new BufferedReader(Channels.newReader(socketChannel, UTF_8));
+
+            System.out.println("Network established");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
